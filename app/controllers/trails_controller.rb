c06079dd58230fcd14b0fe549a8fc0a0603eef9d
Jean Velonis <jean.velonis@gmail.com>
2019-09-05 17:27:04 +0000

    adding fields to the table

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -6,9 +6,13 @@ class TrailsController < ApplicationController
     end
 
     def index
-        puts "--UR IN INDEX FUNCTION ------------------------------------------"
+        puts "--WEATHER API IN INDEX FUNCTION ------------------------------------------"
         @weatherData = HTTParty.get('https://api.openweathermap.org/data/2.5/weather?zip=85213,us&appid=bef3bd0c3ea4da917953e027b5dc2d91')
         #@weatherData.code, @weatherData.message, @weatherData.headers.inspect
+
+        puts "--HIKING API IN INDEX FUNCTION ------------------------------------------"
+        @hikingData = HTTParty.get('https://www.hikingproject.com/data/get-trails?lat=33.4152&lon=-111.8315&maxDistance=10&key=200583144-74f526f47bf91dadd7add272542d8019')
+        
     end
 
     def create
--- db/migrate/20190903175344_create_trails.rb
+++ db/migrate/20190903175344_create_trails.rb
@@ -2,8 +2,11 @@ class CreateTrails < ActiveRecord::Migration[5.2]
   def change
     create_table :trails do |t|
       t.string :name
+      t.string :type
+      t.string :difficulty
+      t.string :location
+      t.text :summary
       t.text :description
-      #t.string :type
       t.timestamps
     end
   end
